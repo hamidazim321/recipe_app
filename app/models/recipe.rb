b6853e26121fd51f6e6441994175dfb8aef33d5a
hamid321 <hamidazim321@gmail.com>
2024-01-29 19:29:57 +0500

    Add model for recipe

---
+++ app/models/recipe.rb
@@ -0,0 +1,6 @@
+class Recipe < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 250}
+  validates :preparation_time, presence: true, numericality: { less_than_or_equal_to: 100 }
+  validates :cooking_time, presence: true, numericality: { less_than_or_equal_to: 100 }
+  validates :description, presence: true
+end
