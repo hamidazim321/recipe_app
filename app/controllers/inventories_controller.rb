ad4e163039cc889acc9a03110fc2bd1df532f6da
hydaline-code <djougancharlen@gmail.com>
2024-01-31 05:28:14 +0100

    fix rubocop

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,14 +1,11 @@
 class InventoriesController < ApplicationController
   before_action :authenticate_user!
-  
 
   def index
     @inventories = Inventory.where(user_id: current_user.id)
   end
 
-  def show
-    
-  end
+  def show; end
 
   def new
     @inventory = Inventory.new
@@ -27,20 +24,17 @@ class InventoriesController < ApplicationController
   def destroy
     @inventory = Inventory.find(params[:id])
     @inventory.destroy
-  
+
     if @inventory.destroyed?
       redirect_to inventories_path, notice: 'Successfully deleted inventory.'
     else
       redirect_back(fallback_location: inventories_path, alert: 'Could not delete inventory.')
     end
   end
-  
 
   private
 
   def inventory_params
     params.require(:inventory).permit(:name, :description).merge(user: current_user)
   end
-  
 end
-
