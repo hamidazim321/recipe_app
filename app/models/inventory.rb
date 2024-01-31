d3539b2148dbf439cd3598df7c605f2b7a1b6270
hydaline-code <djougancharlen@gmail.com>
2024-01-31 04:34:23 +0100

    remove commented code

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,6 +2,5 @@ class Inventory < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
 
-  #has_many :inventory_foods
   validates :description, presence: true, length: { maximum: 250 }
 end
