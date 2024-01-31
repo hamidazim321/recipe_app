9724bf56cb854ea0ff3ce37af86dc03fd11d7046
hydaline-code <djougancharlen@gmail.com>
2024-01-31 04:49:26 +0100

    Add inventory controller

---
+++ app/controllers/inventories_controller.rb
@@ -0,0 +1,47 @@
+class InventoriesController < ApplicationController
+  before_action :authenticate_user!
+  
+
+  def index
+    @inventories = Inventory.where(user_id: current_user.id)
+  end
+
+  def show
+    
+  end
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = Inventory.new(inventory_params)
+
+    if @inventory.save
+      redirect_to inventories_path, notice: 'Inventory created Successfully.'
+    else
+      render :new, alert: 'Could not create inventory.'
+    end
+  end
+
+  def destroy
+    @inventory = Inventory.find(params[:id])
+    @inventory.destroy
+  
+    if @inventory.destroyed?
+      redirect_to inventories_path, notice: 'Successfully deleted inventory.'
+    else
+      render :new, alert: 'Could not delete inventory.'
+    end
+  end
+
+  private
+
+  def inventory_params
+    inventory_hash = params.require(:inventory).permit(:name, :description)
+    inventory_hash[:user] = current_user
+    inventory_hash
+  end
+end
+
+end
