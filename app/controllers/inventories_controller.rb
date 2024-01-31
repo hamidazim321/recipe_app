9e02d1516c417aabe3809e2b87706f2ee5a024b6
hamid321 <hamidazim321@gmail.com>
2024-01-31 20:42:26 +0500

    Fix models

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -24,7 +24,7 @@ class InventoriesController < ApplicationController
   end
 
   def destroy
-    @inventory = Inventory.find(params[:id])
+    @inventory = Inventory.find_by_id(params[:id])
     @inventory.destroy
 
     if @inventory.destroyed?
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   has_many :inventory_foods, dependent: :destroy
+  has_many :inventory, through: :inventory_foods
 
   validates :measurement_unit, presence: true, inclusion: { in: %w[g mg kg], message: '%<value>s is not a valid unit' }
   validates :name, presence: true, length: { maximum: 120 }
