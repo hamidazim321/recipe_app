62d08cdfaccc0273524a9cac0c2cdaa5ecaa0c25
Muhammad Bilal Liaquat <bilal.liaquat7@gmail.com>
2024-02-01 13:16:42 +0500

    Update food.rb
    recipe and recipe foods added

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
 class Food < ApplicationRecord
   has_many :inventory_foods, dependent: :destroy
   has_many :inventory, through: :inventory_foods
+  has_many :recipes,
+  through :recipe_foods
 
   validates :measurement_unit, presence: true, inclusion: { in: %w[g mg kg], message: '%<value>s is not a valid unit' }
   validates :name, presence: true, length: { maximum: 120 }
