5f587b92673df2b6326c0df767e9528cc685a350
Muhammad Bilal Liaquat <bilal.liaquat7@gmail.com>
2024-02-01 13:19:34 +0500

    Update recipe.rb
    food reference added

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :foods,
+  through :recipe_foods
 
   validates :name, presence: true, length: { maximum: 250 }
   validates :preparation_time, presence: true, numericality: { less_than_or_equal_to: 100 }
